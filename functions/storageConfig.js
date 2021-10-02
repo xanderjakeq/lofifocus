7c1f306d18418c98cb2abdab9906224bf075ca64
xanderjakeq <xanderjakeq@gmail.com>
2021-10-02 13:39:29 -0400

    fix: fix storage bucket

--- functions/storageConfig.js
+++ functions/storageConfig.js
@@ -2,6 +2,6 @@ const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage();
 
-const bucket = storage.bucket('lofifocus.appspot.com');
+const bucket = storage.bucket('lofifocus-71c8f.appspot.com');
 
 module.exports = bucket;
